16320f86e5b6635daa4d7a0c021c8e20f0231af0
Alex <alex_mcgarry@hotmail.co.uk>
2023-12-06 17:35:42 +0000

    set up clerk auth

---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from '@clerk/nextjs';
+
+const SignUpPage = () => {
+  return (
+    <div className='h-screen w-screen items-center justify-center'>
+      <SignUp />
+    </div>
+  );
+};
+export default SignUpPage;
