8d09f34b9d54093ba17da746acf3b27f85815234
Alex <alex_mcgarry@hotmail.co.uk>
2023-12-08 12:07:08 +0000

    add editor page

--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -1,5 +1,17 @@
+'use client';
+
 const EntryCard = ({ entry }) => {
-  return <div>{entry.id}</div>;
+  const date = new Date(entry.createdAt).toDateString();
+  return (
+    <div className=" card cursor-pointer overflow-hidden bg-slate-50 text-slate-800 shadow-md px-4 py-2 sm:px-6 ">
+      <div className="card-body divide-y divide-slate-600  py-5 ">
+        <div className="card py-2">{date}</div>
+        <div className="pb-2 pt-4">summary</div>
+
+        <div className="py-3 justify-start">mood</div>
+      </div>
+    </div>
+  );
 };
 
 export default EntryCard;
--- components/NewEntryCard.tsx
+++ components/NewEntryCard.tsx
@@ -1,11 +1,22 @@
 'use client';
 
+import { newEntry } from '@/utils/api';
+import { revalidatePath } from 'next/cache';
+import { useRouter } from 'next/navigation';
+
 const NewEntryCard = () => {
+  const router = useRouter();
+
+
+  const handleOnClick = async () => {
+    const data = await newEntry();
+    router.push(`/journal/${data.id}`);
+  };
   return (
-    <div className="card w-96 cursor-pointer overflow-hidden bg-slate-50 shadow-md px-4 py-5 sm:p-6 ">
-      <div className="card-body">
-        <h2 className="card-title prose">New Entry</h2>
-        <p className=" prose">How are you feeling today?</p>
+    <div className="card cursor-pointer overflow-hidden bg-slate-50 text-slate-800 shadow-md px-4 py-5 sm:p-6 ">
+      <div className="card-body" onClick={handleOnClick}>
+        <h2 className="card-title">New Entry</h2>
+        <p className=" prose">Write about your day</p>
         <div className="card-actions justify-end"></div>
       </div>
     </div>
