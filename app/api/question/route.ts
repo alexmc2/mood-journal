c99b1a4dcb1c5fc7127b9bead81fa01c54803bdd
Alex <alex_mcgarry@hotmail.co.uk>
2023-12-19 15:22:45 +0000

    fix API error for empty content

--- app/api/question/route.ts
+++ app/api/question/route.ts
@@ -3,7 +3,8 @@ import { getUserByClerkId } from '@/utils/auth';
 import { prisma } from '@/utils/db';
 import { NextResponse } from 'next/server';
 
-export const POST = async (request) => {
+export const POST = async (request: { body: any; json: () => PromiseLike<{ question: any; }> | { question: any; }; }) => {
+   console.log(request.body);
   const { question } = await request.json();
   const user = await getUserByClerkId();
   const entries = await prisma.journalEntry.findMany({
@@ -18,5 +19,6 @@ export const POST = async (request) => {
   });
 
   const answer = await qa(question, entries);
+  console.log(answer);
   return NextResponse.json({ data: answer });
 };
--- utils/ai.ts
+++ utils/ai.ts
@@ -43,7 +43,7 @@ const getPrompt = async (content) => {
 
   const prompt = new PromptTemplate({
     template:
-      'Analyse the following journal entry. Follow the instructions and format your response to match the format instructions. \n{format_instructions}\n{entry}.  ',
+      'Analyse the following journal entry. Follow the instructions and format your response to match the format instructions. \n{format_instructions}\n{entry}. You are a friendly, wise psychotherapist',
     inputVariables: ['entry'],
     partialVariables: { format_instructions },
   });
@@ -83,27 +83,34 @@ export const analyse = async (content) => {
 };
 
 export const qa = async (question, entries) => {
-  const docs = entries.map((entry) => {
-    return new Document({
-      pageContent: entry.content,
-      metadata: { id: entry.id, createdAt: entry.createdAt },
+  try {
+    const docs = entries.map((entry) => {
+      return new Document({
+        pageContent: entry.content,
+        metadata: { id: entry.id, createdAt: entry.createdAt },
+      });
     });
-  });
 
-  const model = new OpenAI({ temperature: 0.4, modelName: 'gpt-3.5-turbo' });
+    const model = new OpenAI({ temperature: 0.4, modelName: 'gpt-3.5-turbo' });
 
-  const chain = loadQARefineChain(model);
+    const chain = loadQARefineChain(model);
 
-  const embeddings = new OpenAIEmbeddings();
+    const embeddings = new OpenAIEmbeddings();
 
-  const store = await MemoryVectorStore.fromDocuments(docs, embeddings);
+    console.log('Docs:', docs);
 
-  const relevantDocs = await store.similaritySearch(question);
+    const store = await MemoryVectorStore.fromDocuments(docs, embeddings);
 
-  const res = await chain.call({
-    input_documents: relevantDocs,
-    question: question,
-  });
+    const relevantDocs = await store.similaritySearch(question);
 
-  return res.output_text;
+    const res = await chain.call({
+      input_documents: relevantDocs,
+      question: question,
+    });
+
+    return res.output_text;
+  } catch (error) {
+    console.error('Error in qa function:', error);
+    throw error; // Re-throw the error for higher-level handling
+  }
 };
