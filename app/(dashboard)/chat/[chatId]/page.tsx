99ae4f716e20ed49de3f5f861115754464c7ebf8
Alex <alex_mcgarry@hotmail.co.uk>
2024-02-09 15:45:02 +0000

    fix type error

--- app/(dashboard)/chat/[chatId]/page.tsx
+++ app/(dashboard)/chat/[chatId]/page.tsx
@@ -3,7 +3,11 @@
 import ChatComponent from '../../../../components/chat/ChatComponent';
 import { useRouter, useSearchParams } from 'next/navigation';
 
-export default function ChatIdPage({ params }: { params: { chatId: string } }) {
+interface ChatIdPageParams {
+  chatId?: string;
+}
+
+export default function ChatIdPage({ params }: { params: ChatIdPageParams }) {
   const searchParams = useSearchParams();
   const initialChatId = searchParams.get('chatId') || params?.chatId;
 
