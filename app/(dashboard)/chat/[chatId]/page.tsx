63bfdd830814673eb68450c4367c9bd28ac8a4d4
Alex <alex_mcgarry@hotmail.co.uk>
2024-02-09 16:38:16 +0000

    type error

--- app/(dashboard)/chat/[chatId]/page.tsx
+++ app/(dashboard)/chat/[chatId]/page.tsx
@@ -3,9 +3,9 @@
 import ChatComponent from '../../../../components/chat/ChatComponent';
 import { useRouter, useSearchParams } from 'next/navigation';
 
-export default function ChatIdPage() {
+export default function ChatIdPage({ params }) {
   const searchParams = useSearchParams();
-  const initialChatId = searchParams.get('chatId');
+  const initialChatId = searchParams.get('chatId') || params?.chatId;
 
   return <ChatComponent initialChatId={initialChatId} />;
 }
