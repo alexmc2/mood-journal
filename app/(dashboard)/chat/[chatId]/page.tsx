6f5686b9bd77023ef196882bf6b2f89905be8022
Alex <alex_mcgarry@hotmail.co.uk>
2024-02-09 15:22:33 +0000

    disable eslint

--- app/(dashboard)/chat/[chatId]/page.tsx
+++ app/(dashboard)/chat/[chatId]/page.tsx
@@ -3,7 +3,7 @@
 import ChatComponent from '../../../../components/chat/ChatComponent';
 import { useRouter, useSearchParams } from 'next/navigation';
 
-export default function ChatIdPage({ params }) {
+export default function ChatIdPage({ params }: { params: { chatId: string } }) {
   const searchParams = useSearchParams();
   const initialChatId = searchParams.get('chatId') || params?.chatId;
 
