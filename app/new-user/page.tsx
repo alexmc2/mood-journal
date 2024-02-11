7ba96e2a610b930b50e58dbf492a8fe9c64addac
Alex <alex_mcgarry@hotmail.co.uk>
2024-02-11 15:52:28 +0000

    type error

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,4 +1,3 @@
-
 import { prisma } from '@/utils/db';
 import { auth, currentUser } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
@@ -6,21 +5,25 @@ import { redirect } from 'next/navigation';
 const createNewUser = async () => {
   const user = await currentUser();
   console.log(user);
-  const match = await prisma.user.findUnique({
-    where: {
-      clerkId: user?.id as string,
-    },
-  });
 
-  if (!match) {
-    const newUser = await prisma.user.create({
-      data: {
+  if (user) {
+    const match = await prisma.user.findUnique({
+      where: {
         clerkId: user.id,
-        email: user?.emailAddresses[0].emailAddress,
       },
     });
-    return newUser;
+
+    if (!match) {
+      const newUser = await prisma.user.create({
+        data: {
+          clerkId: user.id,
+          email: user.emailAddresses[0].emailAddress,
+        },
+      });
+      return newUser;
+    }
   }
+
   redirect('/journal');
 };
 
