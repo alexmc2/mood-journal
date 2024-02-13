e3d52c98c10d7f48286475b7b3d1c31c7cac493c
Alex <alex_mcgarry@hotmail.co.uk>
2024-02-13 18:54:53 +0000

    update

--- utils/db.ts
+++ utils/db.ts
@@ -1,28 +1,28 @@
-// import { PrismaClient } from '@prisma/client/edge';
-
-// const globalForPrisma = globalThis as unknown as {
-//   prisma: PrismaClient | undefined;
-// };
-
-// export const prisma =
-//   globalForPrisma.prisma ??
-//   new PrismaClient({
-
-//   });
-
-// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
 import { PrismaClient } from '@prisma/client';
-import { withAccelerate } from '@prisma/extension-accelerate';
 
-const prismaClientSingleton = () => {
-  return new PrismaClient().$extends(withAccelerate());
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
 };
 
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
-}
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
 
-export const prisma = globalThis.prisma ?? prismaClientSingleton();
+  });
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+// import { PrismaClient } from '@prisma/client';
+// import { withAccelerate } from '@prisma/extension-accelerate';
+
+// const prismaClientSingleton = () => {
+//   return new PrismaClient().$extends(withAccelerate());
+// };
+
+// declare global {
+//   var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+// }
+
+// export const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+// if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
