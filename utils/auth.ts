f5cdcd65dadea10e853fa3223a5e71bb07c0ebed
Alex <alex_mcgarry@hotmail.co.uk>
2024-02-13 19:33:21 +0000

    remove edge

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,9 +1,6 @@
 import { auth } from '@clerk/nextjs';
 import { prisma } from './db';
 
-export const runtime = 'edge';
-
-export const dynamic = 'force-dynamic';
 
 export const getUserByClerkId = async () => {
   const { userId } = await auth();
