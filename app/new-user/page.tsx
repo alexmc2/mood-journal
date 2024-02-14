88099c252779084961a20a0f33a41ed9921c35ca
Alex <alex_mcgarry@hotmail.co.uk>
2024-02-14 13:03:43 +0000

    authentication

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,8 +1,6 @@
-
 import { prisma } from '@/utils/db';
 import { auth, currentUser } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
-
 
 const createNewUser = async () => {
   const user = await currentUser();
@@ -35,3 +33,41 @@ const NewUser = async () => {
 };
 
 export default NewUser;
+
+// app/new-user/page.tsx
+// 'use client';
+
+// import { useEffect } from 'react';
+// import { currentUser } from '@clerk/nextjs';
+// import { useRouter } from 'next/navigation';
+// import { prisma } from '@/utils/db';
+
+// export default function NewUser() {
+//   const navigate = useRouter();
+
+//   useEffect(() => {
+//     const createAndRedirectUser = async () => {
+//       const user = await currentUser();
+//       if (user) {
+//         const match = await prisma.user.findUnique({
+//           where: { clerkId: user.id },
+//         });
+
+//         if (!match) {
+//           await prisma.user.create({
+//             data: {
+//               clerkId: user.id,
+//               email: user.emailAddresses[0].emailAddress,
+//             },
+//           });
+//         }
+//       }
+
+//       navigate.push('/journal');
+//     };
+
+//     createAndRedirectUser();
+//   }, [navigate]);
+
+//   return <div>...loading</div>;
+// }
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,36 @@
-import { authMiddleware } from '@clerk/nextjs';
+import {
+  authMiddleware,
+  redirectToSignIn,
+  redirectToSignUp,
+} from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/'],
+  publicRoutes: [
+    '/sign-in/[[...sign-in]]',
+    '/sign-up/[[...sign-up]]',
+    '/new-user',
+    '/',
+  ],
 });
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+// export default authMiddleware({
+//   afterAuth(auth, req, evt) {
+//     // handle users who aren't authenticated
+//     if (!auth.userId && !auth.isPublicRoute) {
+//       if (req.url === '/new-user') {
+//         return redirectToSignUp({ returnBackUrl: '/new-user' });
+//       } else if (req.url === '/journal') {
+//         return redirectToSignIn({ returnBackUrl: '/journal' });
+//       }
+//     }
+//   },
+//   publicRoutes: ['/sign-in/[[...sign-in]]', '/sign-up/[[...sign-up]]', '/new-user', '/'],
+// });
+
+// export const config = {
+//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
