91b76a88f160f5d77f710a36ae2dace4f8d6afaa
Alex <alex_mcgarry@hotmail.co.uk>
2024-02-14 02:50:12 +0000

    deploy

--- utils/db.ts
+++ utils/db.ts
@@ -12,17 +12,3 @@ export const prisma =
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
-// import { PrismaClient } from '@prisma/client';
-// import { withAccelerate } from '@prisma/extension-accelerate';
-
-// const prismaClientSingleton = () => {
-//   return new PrismaClient().$extends(withAccelerate());
-// };
-
-// declare global {
-//   var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
-// }
-
-// export const prisma = globalThis.prisma ?? prismaClientSingleton();
-
-// if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
