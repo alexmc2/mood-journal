8ecab05efa443880a80d5af31b1f9280c3489843
Alex <alex_mcgarry@hotmail.co.uk>
2024-02-14 11:56:08 +0000

    authentication

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -4,11 +4,7 @@ export default function SignUpPage() {
   return (
     <div className="flex justify-center py-40 px-12">
       <SignUp
-        path="/sign-up"
-        routing="path"
-        signInUrl="/sign-in"
-        redirectUrl="/new-user"
-        afterSignUpUrl="/new-user"
+       
       />
     </div>
   );
