be580682990a2b24e76d6314eab9bd9399020f4c
Alex <alex_mcgarry@hotmail.co.uk>
2024-02-24 21:39:29 +0000

    update metadata tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,43 +32,41 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
-      <Head>
-        <meta property="og:url" content="http://mood-chat.com" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Mood Journal" />
-        <meta
-          property="og:description"
-          content="AI-Powered Mood Tracking Journal"
-        />
-        <meta
-          property="og:image"
-          content="https://res.cloudinary.com/drbz4rq7y/image/upload/v1708799841/Screenshot_from_2024-02-24_18-33-22_vuuw8j.png"
-        />
-        <title>{metadata.title}</title>
-        <meta name="description" />
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={metadata.title} />
-        <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content={metadata.imageUrl} />
-      </Head>
+    <html lang="en" suppressHydrationWarning>
+      <head>
+          <meta property="og:url" content="http://mood-chat.com" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Mood Journal" />
+          <meta
+            property="og:description"
+            content="AI-Powered Mood Tracking Journal"
+          />
+          <meta
+            property="og:image"
+            content="https://res.cloudinary.com/drbz4rq7y/image/upload/v1708799841/Screenshot_from_2024-02-24_18-33-22_vuuw8j.png"
+          />
+          <title>{metadata.title}</title>
+          <meta name="description" />
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content={metadata.title} />
+          <meta name="twitter:description" content={metadata.description} />
+          <meta name="twitter:image" content={metadata.imageUrl} />
+        </head>
 
-      <html lang="en">
-        <body
-          className={`${inter.className} font-inter antialiased bg-neutral-100 dark:bg-blue-800 text-slate-600 dark:text-slate-300 no-scrollbar`}
-        >
-          <ClerkProvider>
-            <Theme>
-              <AppProvider>
-                <Providers>
-                  {children} <Analytics />
-                </Providers>
-              </AppProvider>
-            </Theme>
-          </ClerkProvider>
-        </body>
-      </html>
-    </>
+      <body
+        className={`${inter.className} font-inter antialiased bg-neutral-100 dark:bg-blue-800 text-slate-600 dark:text-slate-300 no-scrollbar`}
+      >
+        <ClerkProvider>
+          <Theme>
+            <AppProvider>
+              <Providers>
+                {children} <Analytics />
+              </Providers>
+            </AppProvider>
+          </Theme>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
